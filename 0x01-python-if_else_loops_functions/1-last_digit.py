53c5d4903c4c1ee820ef0408901ae0f16e3bf3aa
Tiamiyu Abdussomad Olanrewaju <coder@Coder.coder.com>
2022-11-29 21:07:30 +0100

    Last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if number < 0:
 else:
     mod = abs(number) % 10
 string = f"Last digit of {number} is {mod} "
-if number % 10 == 0:
+if mod == 0:
     string = string + "and is 0"
-elif (abs(number) % 10) > 5:
+elif mod > 5:
     string = string + "and is greater than 5"
 else:
     string = string + "and is less than 6 and not 0"
