e855266b59599d650cb65a8e5aa23716fcd9dae2
Tiamiyu Abdussomad Olanrewaju <coder@Coder.coder.com>
2022-11-30 23:21:41 +0100

    Insert linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,7 +14,10 @@ listint_t *insert_node(listint_t **head, int number)
 	new->n = number;
 	new->next = NULL;
 	if (*head == NULL)
+	{
+		*head = new;
 		return (new);
+	}
 	if (buff->n > number)
 	{
 		new->next = buff;
