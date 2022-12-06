792c486e84cf19e8383964dd7d4e06c7ec3f7e8b
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-06 06:30:22 +0100

    Print matrix number

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,6 @@ def print_reversed_list_integer(my_list=[]):
     x = my_list[:]
     x.reverse()
     if len(my_list) == 0:
-        print("")
         return
     for i in x:
         print("{:d}".format(i))
