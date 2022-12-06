84bb197b0622e1c720c4369bc0148ee9167e34a3
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-06 06:26:16 +0100

    Print matrix number

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if (len(matrix) == 1 and len(matrix[0]) == 0) or len(matrix) == 0:
         print("")
+        return
     for i in matrix:
         for j in range(len(i)):
             if j == len(i) - 1:
