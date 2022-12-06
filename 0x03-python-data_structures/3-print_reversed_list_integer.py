ef9e0b2cd9b18ed7d5aad362fbfb1221a04ce605
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-06 06:50:58 +0100

    Divisible by 2

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    x = my_list[:]
-    x.reverse()
-    if len(my_list) == 0:
-
-    for i in x:
-        print("{:d}".format(i))
+    for i in reversed(range(len(my_list))):
+        print("{:d}".format(my_list[i]))
