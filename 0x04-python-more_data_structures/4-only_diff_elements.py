638f39c0c6762e88af524ff44292ef740b63e2f5
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-07 21:09:51 +0100

    Different elements only

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    ret  = set([])
+    for x in set_1:
+        if x not in set_2:
+            ret.add(x)
+    for y in set_2:
+        if y not in set_1:
+            ret.add(y)
+    return ret
