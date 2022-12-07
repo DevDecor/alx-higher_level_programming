93f18a4defae23b5226caaa1e3380fde2ea837e7
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-07 22:19:49 +0100

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    m = a_dictionary.values()[0]
+    m = list(a_dictionary.values())[0]
     best = ""
     for i in a_dictionary:
-        if a_dictionary[i] > m:
-            m = a_dictionary[i]
-            best = i
-    return i
+        if max(list(a_dictionary.values())) == a_dictionary[i]:
+            return i
+    return None
