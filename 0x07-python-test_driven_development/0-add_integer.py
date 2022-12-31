36b3240c9a87f4394b0206771b103a213aa6c792
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-31 20:52:32 +0100

    Add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """This function adds two integers
+    
+    Args:
+        a (int): First number
+        b (int): Second number
+    Raises:
+        TypeError: Iro lasan
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a, b = int(a), int(b)
+    return a + b
