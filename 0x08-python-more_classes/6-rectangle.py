977fbda7fba59008d17d927fd20231bd9dafabc4
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2023-01-02 23:46:40 +0100

    Rectangle class

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,9 @@
 
 class Rectangle:
     """Rectangle defined"""
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         self.width, self.height = width, height
 
     @property
