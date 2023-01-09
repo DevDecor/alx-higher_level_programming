c7024ebe8f9e9219cb1c078297dd2477a33db9d9
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2023-01-09 23:26:28 +0100

    Is instance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class BaseGeometry:
+    """Base geometry"""
+    def area(self):
+        """Area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """func"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
