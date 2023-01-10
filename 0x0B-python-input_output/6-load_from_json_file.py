059e6c734cc0102fcf8d6884679d6ad744f72c8c
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2023-01-10 22:14:35 +0100

    JSON representation

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,6 +7,6 @@ import json
 def load_from_json_file(filename):
     """Prints file content"""
     with open(filename, 'w') as f:
-        x = json.loads(f)
+        x = json.loads(f.read())
     f.close()
     return x
