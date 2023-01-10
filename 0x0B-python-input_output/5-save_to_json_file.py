3dacfa3b5cbaa7b0ab3eb19d8fb75249078cfec1
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2023-01-10 22:10:43 +0100

    JSON representation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python module"""
+import json
+"""Python json module"""
+
+
+def save_to_json_file(my_obj, filename):
+    """Prints file content"""
+    with open(filename, 'w') as f:
+        x = f.write(json.dumps(my_obj))
+    f.close()
+    return x
