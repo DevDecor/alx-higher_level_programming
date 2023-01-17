0ed4353f2552b181b1e7fc6c73e1f9642bca0fa6
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2023-01-17 20:29:15 +0100

    Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python module"""
+
+
+class Base:
+    """Python class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Python Function"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""Python module"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Python class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Python methon"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Class method"""
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        """Class method"""
+        if (not isinstance(w, int)):
+            raise TypeError("width must be an integer")
+        if w <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = w
+
+    @property
+    def height(self):
+        """Class method"""
+        return self.__height
+
+    @height.setter
+    def height(self, h):
+        """Class method"""
+        if (not isinstance(h, int)):
+            raise TypeError("height must be an integer")
+        if h <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = h
+
+    @property
+    def x(self):
+        """Class method"""
+        return self.__x
+
+    @x.setter
+    def x(self, p):
+        """Class method"""
+        if (not isinstance(p, int)):
+            raise TypeError("x must be an integer")
+        if p < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = p
+
+    @property
+    def y(self):
+        """Class method"""
+        return self.__y
+
+    @y.setter
+    def y(self, q):
+        """Class method"""
+        if (not isinstance(q, int)):
+            raise TypeError("y must be an integer")
+        if q < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = q
+
+    def area(self):
+        """Area of the rectangle"""
+        return self.width * self.height
+
+    def display(self):
+        """display the rectangle"""
+        y = 0
+        for i in range(self.height + self.y):
+            x = 0
+            if y < self.y:
+                print('')
+                y = y + 1
+                continue
+            for j in range(self.width + self.x):
+                if x < self.x:
+                    print(' ', end='')
+                else:
+                    print('#', end='')
+                x = x + 1
+            y = y + 1
+            print('')
+
+    def __str__(self):
+        """String"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
