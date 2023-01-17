08ccfbb6607e5f04a88350af7d541a1c1a770414
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2023-01-17 22:34:53 +0100

    Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python module"""
+from rectangle import Rectangle
+"""Rectangle"""
+
+
+class Square(Rectangle):
+    """Class documentation"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String overloading"""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.size)
