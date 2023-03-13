d5ada577d1843dd29c9944527a3b5d7626fc845d
DevDecor <fiyiziyech@gmail.com>
2023-03-13 17:18:09 -0400

    Javascript C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const arg = parseInt(process.argv[2]);
+if (!arg) console.log('Missing number of occurrences');
+else if (arg < 1) ;
+else {
+  for (let i = 0; i < arg; i++) console.log('C is fun');
+}
