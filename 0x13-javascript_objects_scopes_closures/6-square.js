aa1107e61cc3a038790bb833ca18877d5212c548
DevDecor <fiyiziyech@gmail.com>
2023-03-14 17:13:22 -0400

    Javascript OOP other madatory

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) return this.print();
     for (let i = 0; i < this.width; i++) {
