4e20bde5c7ab990ab6229bf442a42f56623cbd7b
DevDecor <fiyiziyech@gmail.com>
2023-03-14 21:17:48 -0400

    Javascript OOP other madatory

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,14 +2,14 @@
 const dict = require('./101-data').dict;
 
 const totalist = Object.entries(dict);
-const vals = Object.values(dict).map(e=>e.toString());
+const vals = Object.values(dict).map(e => e.toString());
 const valsUniq = [...new Set(vals)];
 const newDict = {};
-valsUniq.sort().forEach(e=>{
+valsUniq.sort().forEach(e => {
   newDict[e] = [];
 });
-totalist.map(e=>e.map(f=>f.toString())).forEach(e=>{
+totalist.map(e => e.map(f => f.toString())).forEach(e => {
   newDict[e[1]].push(e[0]);
   newDict[e[1]].sort();
-})
+});
 console.log(newDict);
