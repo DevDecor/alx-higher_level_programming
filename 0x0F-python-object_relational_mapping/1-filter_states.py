abd396272d2e2d7148a5f10bdf40d905ee0d6475
DevLarry <olanrewajusamad070@gmail.com>
2023-03-17 22:17:14 +0100

    Filter search

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@
 import MySQLdb
 import sys
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost',
+    db = MySQLdb.connect(host='localhost:3306',
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states WHERE name REGEXP '^N'
