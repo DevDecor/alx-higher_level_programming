5a0e119ccf3f8aab089af9e2a77a78dbab3deabd
DevLarry <olanrewajusamad070@gmail.com>
2023-03-18 17:30:24 +0100

    hbtn_0e_4_usa cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Mysql module that allows you to select costom state
+ Usage: ./2-filter_class: <user>
+                            <password>
+                            <database>
+                            <filter>
+"""
+
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                   JOIN states
+                   ON cities.state_id = states.id
+                   ORDER BY cities.id ASC;""")
+    rows = cursor.fetchall()
+
+    print(', '.join([row[1] for row in rows if row[2]==sys.argv[4]]))
+
