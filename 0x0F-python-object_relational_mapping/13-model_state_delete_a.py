31bfb3887b370227fc65d60b18280d9243659010
DevLarry <olanrewajusamad070@gmail.com>
2023-03-18 23:57:27 +0100

    SQLAlchemy update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
                                    sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)()
-    users = Session.query(State).all()
+    users = Session.query(State).filter(State.name.like('%a%')).all()
     for user in users:
-        if user and 'a' in user.name:
-            Session.delete(user)
+        Session.delete(user)
     Session.commit()
