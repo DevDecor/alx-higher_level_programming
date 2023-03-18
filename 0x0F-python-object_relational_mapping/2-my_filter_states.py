4473055e56c1c6cf8c39b42baee343beec5c2533
DevLarry <olanrewajusamad070@gmail.com>
2023-03-18 15:22:48 +0100

    Filter search

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""Mysql module"""
+"""Mysql module that allows you to select costom state
+ Usage: ./2-filter_class: <user>
+                            <password>
+                            <database>
+                            <filter>
+"""
 
 import MySQLdb
 import sys
@@ -7,8 +12,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name='{}'
-                   ORDER BY id ASC;""".format(sys.argv[4]))
+    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'
+                   ORDER BY id ASC;""".format(sys.argv[4].strip("'")))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
