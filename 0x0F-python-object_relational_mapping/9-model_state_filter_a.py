21080feaf9f084e92a500336e65bfda5b2829113
DevLarry <olanrewajusamad070@gmail.com>
2023-03-18 23:10:32 +0100

    SQLAlchemy get filtered

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)()
+    users = Session.query(State).all()
+    for user in users:
+        if 'a' in user.name:
+            print(f'{user.id}: {user.name}')
