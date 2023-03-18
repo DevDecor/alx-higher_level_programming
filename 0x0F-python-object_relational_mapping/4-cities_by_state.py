c7dec9b326dfdc9c5677d92cc1ad55252483939a
DevLarry <olanrewajusamad070@gmail.com>
2023-03-18 16:42:16 +0100

    Cities we know

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Mysql module that allows you to select costom state
+ Usage: ./2-filter_class: <user>
+                            <password>
+                            <database>
+                            <filter>
+"""
+
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM cities;""")
+    rows = cursor.fetchall()
+    [print(row) for row in rows]
