65f7bf01fbaeffed7f1410975563d449ae6f5887
DevLarry <olanrewajusamad070@gmail.com>
2023-03-18 22:16:25 +0100

    SQLAlchemy create table

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 class State(Base):
     """States class"""
     __tablename__ = 'states'
