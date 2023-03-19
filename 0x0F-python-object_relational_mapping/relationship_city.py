1128eef6796e59c7da8886b735d85b041a501a5b
DevLarry <olanrewajusamad070@gmail.com>
2023-03-19 15:38:11 +0100

    hbtn_0e_14_usa Relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""use SQLAlchemy
+
+
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    """States class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+    state = relationship(State, back_populates='cities')
+
+
+State.cities = relationship(City, order_by=City.id, back_populates="state")
