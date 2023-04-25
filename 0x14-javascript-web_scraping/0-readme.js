c67b819cf66c9f52ce677cfd0a162c42f4c6c9e8
DevLarry <olanrewajusamad070@gmail.com>
2023-04-25 13:30:20 +0100

    Read file content

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Reads the content of a file
+const fs = require('fs')
+
+
+fs.readFile(process.argv[2], 'utf-8', (err, data)=>{
+  if (err) return console.log(err);
+  console.log(data)
+})
