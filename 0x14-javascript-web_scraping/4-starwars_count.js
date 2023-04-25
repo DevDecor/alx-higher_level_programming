ebe34b19e54eb3e20307ca5625594cb5d41f8302
DevLarry <olanrewajusamad070@gmail.com>
2023-04-25 20:40:59 +0100

    All that is supposed to be done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,9 @@
 // Reads the content of a file
 const request = require('request');
 
-request(`https://swapi-api.alx-tools.com/api/people/${18}`, (err, data, body) => {
+request(`https://swapi-api.alx-tools.com/api/films`, (err, data, body) => {
   if (err) return console.log(err);
-  console.log(JSON.parse(body).films.length);
+  console.log(JSON.parse(body).results.filter(e=>{
+    return e.characters.includes("https://swapi-api.alx-tools.com/api/people/18/");
+  }).length);
 });
