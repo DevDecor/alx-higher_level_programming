ad36ed5248361a5404229e61ce7950534292fdf1
DevDecor <fiyiziyech@gmail.com>
2023-07-07 00:42:54 +0100

    Post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request, parse
+import sys
+
+if __name__ == "__main__":
+    data = parse.urlencode({"email": sys.argv[2]}).encode()
+    req = request.Request(sys.argv[1], data=data)
+    with request.urlopen(req) as response:
+        """Fires a get request"""
+        print(str(response.read, 'utf-8'))
