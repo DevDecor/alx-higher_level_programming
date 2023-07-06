d7e8d45d1e01c3f2297059c4491d1065f08896f4
DevDecor <fiyiziyech@gmail.com>
2023-07-06 23:45:05 +0100

    Fetch status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request as request
 url = 'https://alx-intranet.hbtn.io/status'
 with request.urlopen(url) as res:
     temp = f"""Body response:
-    - type: {type(res.read())}
-    - content: {res.read()}
-    - utf8 content: {str(res.read(), 'utf-8')}"""
+\t- type: {type(res.read())}
+\t- content: {res.read()}
+\t- utf8 content: {str(res.read(), 'utf-8')}"""
     print(temp)
