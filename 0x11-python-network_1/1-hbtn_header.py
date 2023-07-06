dda129159390c929bb667ae9aa2b2f60893d8380
DevDecor <fiyiziyech@gmail.com>
2023-07-07 00:19:45 +0100

    get header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
-
-import urllib.request as request
+import urllib.request
 import sys
 
-with request.urlopen(sys.argv[1]) as response:
+with urllib.request.urlopen(sys.argv[1]) as response:
     """Fires a get request"""
     print(dict(response.info())['X-Request-Id'])
