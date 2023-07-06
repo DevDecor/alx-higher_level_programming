d4272740084aff0ef6ed4b8ab865916e0b1af6a7
DevDecor <fiyiziyech@gmail.com>
2023-07-07 00:16:02 +0100

    get header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,4 @@ import sys
 
 with request.urlopen(sys.argv[1]) as response:
     """Fires a get request"""
-    res = dict(response.info())['X-Request-Id']
-    # [x for x in response.info().split('\n') if 'X-Request-Id' in x][0]
-    print(res)
+    print(dict(response.info())['X-Request-Id'])
